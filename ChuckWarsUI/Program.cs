e3ae651a417852af481a8ebdcc3f1c0aacf3908f
Nawa Mumbwe <nawa@nml.co.za>
2023-02-12 22:14:32 +0200

    test base url

--- ChuckWarsUI/Program.cs
+++ ChuckWarsUI/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiConfig = new ChuckWarsApiConfiguration();
 builder.Configuration.GetSection(ChuckWarsApiConfiguration.SectionName).Bind(apiConfig);
-builder.Services.AddScoped<IClient, Client>(sp => new Client(apiConfig.BaseUrl));
+builder.Services.AddScoped<IClient, Client>(sp => new Client("https://chuck-wars-api.azurewebsites.net"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
