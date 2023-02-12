d496821e0c77bbc83ecaad2fe0138b76555857e6
Nawa Mumbwe <nawa@nml.co.za>
2023-02-12 21:15:21 +0200

    add ui project

---
+++ ChuckWarsUI/Configuration/ChuckWarsApiConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ChuckWarsUI.Configuration
+{
+    public class ChuckWarsApiConfiguration
+    {
+        public static string SectionName = "ChuckWarsApi";
+
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ ChuckWarsUI/Program.cs
@@ -0,0 +1,16 @@
+using ChuckWarsUI;
+using ChuckWarsUI.Configuration;
+using ChuckWarsUI.Integration.ChuckWarsApi;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+var apiConfig = new ChuckWarsApiConfiguration();
+builder.Configuration.GetSection(ChuckWarsApiConfiguration.SectionName).Bind(apiConfig);
+builder.Services.AddScoped<IClient, Client>(sp => new Client(apiConfig.BaseUrl));
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
